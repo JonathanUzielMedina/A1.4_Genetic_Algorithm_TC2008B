f47294103bc832afb3d6f8ddbc4a4f888b0d5eaa
JonathanUzielMedina <A01255048@tec.mx>
2025-08-26 15:41:55 -0700

    Base (incompleta)

---
+++ A1.4_TC2008B.py
@@ -0,0 +1,27 @@
+import random as r
+import numpy as np
+
+## DISCLAIMER: Solo es una BASE no se ha implementado código completo aún.
+
+poblacion = np.zeros((15, 1))
+
+def inicializar(t: int):
+    pass
+
+def seleccionar(t: int):
+    pass
+
+def evaluar(t: int):
+    pass
+
+def mutacion(t: int):
+    pass
+
+def recombinar(t: int):
+    pass
+
+if __name__ == "__main__":
+  t = 0
+  parada = False
+  while not parada:
+      t += 1
