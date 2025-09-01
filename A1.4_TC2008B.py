78894ba7371d369bbd3dd5f7b114549e0b83668d
eduardocardenasv27 <a00232432@tec.mx>
2025-09-01 12:29:18 -0600

    Añadio funcion de mutar a A1.4_TC2008B.py

--- A1.4_TC2008B.py
+++ A1.4_TC2008B.py
@@ -1,3 +1,22 @@
+"""
+A1.4 Genetic Algorithms (GAs)
+
+Modelación de Sistemas Multiagentes y Gráficas Computacionales (TC2008B).
+
+Equipo:
+    - Jesús Felipe Bastidas Valenzuela (A01255206)
+    - Sebastián Blachet Sánchez (A00227588)
+    - Eduardo Cárdenas Valadez (A002332432)
+    - Jonathan Uziel Medina Rodríguez (A01255048)
+    - Araceli Ruiz Sánchez (A01255302)
+
+Docente: Dr. Edgar León Sandoval.
+
+Fecha de entrega: 1 de septiembre de 2025.
+________________________________________________________________________
+Descripción: Implementación del algoritmo genético.
+"""
+
 import numpy as np
 import random as r
 
@@ -20,9 +39,18 @@ def seleccionar(t: int):
 
 def evaluar(t: int):
     pass
-
-def mutacion(t: int):
-    pass
+    
+def mutacion(poblacion, prob):
+    newP = []
+    for parte in poblacion:
+        newparte = []
+        for bit in parte:
+            if r.random() < prob:
+                newparte.append(1 - bit) 
+            else:
+                newparte.append(bit)
+        newP.append(newparte)
+    return newP
 
 def recombinar(t: int):
     pass
@@ -32,3 +60,11 @@ if __name__ == "__main__":
   parada = False
   while not parada:
       t += 1
+
+
+"""
+Referencias:
+
+- Huddar, M. [Mahesh Huddar]. (2024, 20 abril). Selection Operators Roulette wheel ranking tournament selection in genetic algorithm Mahesh Huddar [Video]. YouTube. Recuperado el 30 de agosto de 2025, de https://www.youtube.com/watch?v=uRF7xSQwNeU
+- NeuralNine. (2024, 2 abril). Genetic Algorithms in Python - Evolution for optimization [Video]. YouTube. Recuperado el 30 de agosto de 2025, de https://www.youtube.com/watch?v=CRtZ-APJEKI
+"""
