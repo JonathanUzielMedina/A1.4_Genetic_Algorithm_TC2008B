649beed35057c5f6360ac405d2cae606cb9fecd8
Jesus Felipe Bastidas Valenzuela <203928100+JFelipeBastidasV@users.noreply.github.com>
2025-09-02 04:20:51 +0000

    version para entrega

--- A1.4_TC2008B.py
+++ A1.4_TC2008B.py
@@ -16,26 +16,21 @@ Fecha de entrega: 1 de septiembre de 2025.
 ________________________________________________________________________
 Descripción: Implementación del algoritmo genético.
 """
-
 import numpy as np
 import random as r
-
-#alternativamente sin numpy
-#p_inicial2 = [[r.randint(0,1) for _ in range(p_binary)] for _ in range(p_size)]
 
 def b2d(b_num):
     return int("".join(str(i) for i in b_num),2)
 
 def evaluar(poblacion):
-    newP = sorted(poblacion, key=b2d, reverse=False)
+    # Ordenar de menor a mayor (más óptimos primero, más cerca de 0)
+    newP = sorted(poblacion, key=b2d)
     return newP
 
-# Seleccionar un individuo por competencia (tournament selection).
 def seleccionarCompetencia(poblacion: list, fitness_poblacion: list, K: int):
-    seleccion_temp = []     # Selección de genomas aleatorios.
-    indices_usados = []     # Índices utilizados (para no repetirse en la aleatoriedad).
-
-    for i in range(K):
+    seleccion_temp = []
+    indices_usados = []
+    for _ in range(K):
         idx = r.randint(0, len(poblacion)-1)
         if idx not in indices_usados:
             seleccion_temp.append((poblacion[idx], fitness_poblacion[idx]))
@@ -47,21 +42,10 @@ def seleccionarCompetencia(poblacion: list, fitness_poblacion: list, K: int):
                     seleccion_temp.append((poblacion[idx], fitness_poblacion[idx]))
                     indices_usados.append(idx)
                     break
-    padre = max(seleccion_temp)[0]  # Seleccionar el genoma más apto como padre.
+    # Seleccionar el genoma más apto como padre (menor fitness)
+    padre = min(seleccion_temp, key=lambda x: x[1])[0]
     return padre
 
-
-# Seleccionar un individuo por ruleta.
-def seleccionarRuleta(poblacion: list, fitness_poblacion: list):
-    sumaFitness = sum(fitness_poblacion)
-    numeroRandom = int(r.randint(0, sumaFitness))
-    actual = 0
-
-    for i in range(len(poblacion)):
-        actual += fitness_poblacion[i]
-        if actual > numeroRandom:
-            return poblacion[i]
-    
 def mutacion(poblacion, prob):
     newP = []
     for parte in poblacion:
@@ -74,9 +58,9 @@ def mutacion(poblacion, prob):
         newP.append(newparte)
     return newP
 
-def recombinar(poblacion: list, p1, p2, p_size):
+def recombinar(poblacion: list, p1, p2, p_size, p_binary, combination_rate):
     nueva = []
-    for i in range(p_size // 2):
+    if r.random() < combination_rate:
         punto_cruce = r.randint(1, p_binary-1)
         h1 = p1[:punto_cruce] + p2[punto_cruce:]
         h2 = p2[:punto_cruce] + p1[punto_cruce:]
@@ -85,35 +69,56 @@ def recombinar(poblacion: list, p1, p2, p_size):
     poblacion = nueva[:p_size]
     return poblacion
 
+def elitismo(poblacion, fitness, num_elite):
+    individuos = []
+    elite_indices = [0] * len(poblacion)
+    elite_indices.sort(key=lambda x: fitness[x])
+    elite_indices = elite_indices[:num_elite]
+
+    for i in elite_indices:
+        individuos.append(poblacion[i])
+    return individuos
+
 if __name__ == "__main__":
     t = 0
     p_size = 7
     p_binary = 4
-    generations = 8
-    p_mut = 0.08
-    p_inicial = np.random.randint(2,size= (p_size,p_binary))
-    
-    evaluar(p_inicial)
-    fitness = [] # Valores de aptitud de los genomas
-    for g in range(len(p_inicial)):
-        fitness.append(int(b2d(p_inicial[g]) ** 2))
-        
-    parada = False
+    generations = 5
+    p_mut = 0.02
+    num_elite = 2  # Número de individuos élite a conservar
+    combination_rate = 0.8
+
+    p_inicial = np.random.randint(2, size=(p_size, p_binary)).tolist()
+    p_inicial = evaluar(p_inicial)
+
     while t < generations:
         t += 1
-        p1 = seleccionarRuleta(p_inicial, fitness)
-        p2 = seleccionarRuleta(p_inicial, fitness)
-        '''felipe arregla tu funcion'''
-        #p_inicial = recombinar(p_inicial, p1, p2, p_size)
-        p_inicial = mutacion(p_inicial, p_mut)
-        evaluar(p_inicial)
-        
-        for g in range(len(p_inicial)):
-            fitness.append(int(b2d(p_inicial[g]) ** 2))
-        
+        fitness = [int(b2d(ind) ** 2) for ind in p_inicial]
+
+        # Elitismo: conservar los mejores individuos (más cercanos a 0)
+        elite = elitismo(p_inicial, fitness, num_elite)
+
+        # Selección y recombinación para el resto de la población
+        hijos = []
+        while len(hijos) < (p_size - num_elite):
+            p1 = seleccionarCompetencia(p_inicial, fitness, K=3)
+            p2 = seleccionarCompetencia(p_inicial, fitness, K=3)
+            # Recombinación usando la función recombinar
+            hijos_parciales = recombinar([], p1, p2, 2, p_binary, combination_rate)
+            for h in hijos_parciales:
+                if len(hijos) < (p_size - num_elite):
+                    hijos.append(h)
+
+        # Mutación
+        hijos = mutacion(hijos, p_mut)
+
+        # Nueva población: élite + hijos
+        p_inicial = elite + hijos
+        p_inicial = evaluar(p_inicial)
+        fitness = [int(b2d(ind) ** 2) for ind in p_inicial]
+
         print(f"Generacion {t}: ")
         for x in p_inicial:
             print(x)
 
-        
         
